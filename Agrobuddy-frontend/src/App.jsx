cb0389871b10865f8501d212617deac5850dd883
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-07-22 16:07:33 +0530

    update it on App JSX

--- Agrobuddy-frontend/src/App.jsx
+++ Agrobuddy-frontend/src/App.jsx
@@ -5,16 +5,24 @@ import Home from './pages/Home';
 import Recommend from './pages/Recommend';
 import Disease from './pages/Disease';
 import NotFound from './pages/NotFound';
+import Marquee from './components/Marquee';
 import './App.css';
 import './index.css';
 
 function App() {
   const [selectedLanguage, setSelectedLanguage] = useState('English');
+  const [selectedState, setSelectedState] = useState('');
 
   return (
     <Router>
-      <div className=" min-h-screen min-w-full bg-black text-white">
-        <Header selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} />
+      <div className=" min-h-screen min-w-full bg-gray-900 text-white">
+        <Header 
+          selectedLanguage={selectedLanguage} 
+          setSelectedLanguage={setSelectedLanguage} 
+          selectedState={selectedState} 
+          setSelectedState={setSelectedState} 
+        />
+        <Marquee state={selectedState} />
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
